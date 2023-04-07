c12815de91e1f535c3b8eca1d205947cf9df0421
Muhammmad Ahsan <ahsangmi91@gmail.com>
2023-04-07 23:25:12 +0500

    changes

---
+++ README.md
@@ -0,0 +1,41 @@
+=> To create tsconfig.json file 
+
+use command tsc --init
+
+=> To create package.json file
+
+use command npm init -y
+add "type": "module",
+  "bin": {
+    "calculator": "index.js"
+  } in package.json
+
+=> To create package-lock.json
+
+use command install npm 
+
+=> To download inquirer
+
+use command npm i inquirer
+
+=> To download chalk
+
+use command npm i chalk
+
+=> To download chalk animation
+
+use command npm i chalk-animation
+
+=> To download figlet
+
+use command npm i figlet
+
+=> for devdepencies of figlet and inquirer
+
+use command npm i --save-dev @types/figlet
+& npm i --save-dev @types/inquirer
+
+=> TO check version of node
+
+use command node -v
+
---
+++ index.ts
@@ -0,0 +1,47 @@
+import inquirer from "inquirer";
+import figlet from "figlet";
+import chalk from "chalk";
+import { sum, division, multiply, subtract } from "./operation.js";
+
+const WELCOME = async () => {
+  console.log(figlet.textSync("Calculator"));
+
+  const input = await inquirer.prompt([
+    {
+      type: "number",
+      name: "num1",
+      message: "Enter First Number",
+    },
+    {
+      type: "number",
+      name: "num2",
+      message: "Enter Second Number",
+    },
+    {
+      type: "list",
+      name: "operation",
+      message: "Select Your Operation",
+      choices: ["+", "/", "-", "*"],
+    },
+  ]);
+
+  if (input.operation === "+") {
+    const result = sum(input.num1, input.num2);
+    console.log(chalk.bgGreen(`result: ${result}`));
+  } else if (input.operation === "/") {
+    const result = division(input.num1, input.num2);
+    console.log(chalk.bgGreen(`result: ${result}`));
+  } else if (input.operation === "-") {
+    const result = subtract(input.num1, input.num2);
+    console.log(chalk.bgGreen(`result: ${result}`));
+  } else if (input.operation === "*") {
+    const result = multiply(input.num1, input.num2);
+    console.log(chalk.bgGreen(`result: ${result}`));
+  } else {
+    console.log("Invalid operation");
+  }
+};
+
+(async () => {
+  await WELCOME();
+})();
